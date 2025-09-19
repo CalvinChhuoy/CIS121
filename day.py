85ba9890a5c8b77b2363bdc602e6f5dd4621d987
CalvinChhuoy <ys0111hw@go.minnstate.edu>
2025-09-19 14:53:48 -0500

    Create day.py
    hi

---
+++ day.py
@@ -0,0 +1,42 @@
+sum_number = 0 
+
+user_number = int(input("Pick a number & type q to stop: "))
+if user_number == "q":
+        sum_number  += int(user_number)
+print(f"Your sum is {sum_number}")
+
+    
+
+
+
+'''
+user_number = int(input("Pick a number: "))
+
+for number in range (5,user_number):
+    if number % 2 == 1:
+        print(number)
+    number += 1
+'''
+'''
+number = 1 
+while number <= 10:
+    if number % 2 == 0:
+        print (number)
+    number += 1
+'''
+'''
+start_number = 5
+user_number = int(input("Enter a number: "))
+
+while start_number <= user_number:
+    if start_number % 2 == 1:
+        print(start_number)
+    start_number += 1
+'''
+'''
+number = 1
+while number <= 10:
+    if number % 2 == 0:
+        print(number)
+    number += 1
+'''
